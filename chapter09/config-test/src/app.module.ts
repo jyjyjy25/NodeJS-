386d7a61bf308141d84fd0ed18623ef1657ba1bc
jyjyjy25 <jyjyjy7418@gmail.com>
2023-07-30 17:32:27 +0900

    [ADD] 여러 환경 변수 파일 사용하기

--- chapter09/config-test/src/app.module.ts
+++ chapter09/config-test/src/app.module.ts
@@ -4,8 +4,17 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { WeatherModule } from './weather/weather.module';
 
+console.log('env: ' + process.env.NODE_ENV); // 서버 기동 시 환경 변수 출력
+console.log('current wroking directory: ' + process.cwd()); // 현재 디렉터리 출력
+
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), WeatherModule], // configModule 설정
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `${process.cwd()}/envs/${process.env.NODE_ENV}.env`,
+    }),
+    WeatherModule,
+  ], // configModule 설정
   controllers: [AppController],
   providers: [AppService],
 })
